1c35994809f35f4f539a2eeb5bf3971ae0b6b2d5
Erick Panni <erickpanni@icloud.com>
2022-12-20 19:55:31 -0300

    Test[Notification entity]: Closing Tests

--- src/application/entities/notification/notification.spec.ts
+++ src/application/entities/notification/notification.spec.ts
@@ -12,12 +12,4 @@ describe('Notification', () => {
         }),
     ).toBeTruthy();
   });
-
-  //   it('Should not be able to create a notification content with less than 5 characters', () => {
-  //     expect(() => new Content('aaa')).toThrow();
-  //   });
-
-  //   it('Should not be able to create a notification content with more than 240 characters', () => {
-  //     expect(() => new Content('a'.repeat(241))).toThrow();
-  //   });
 });
