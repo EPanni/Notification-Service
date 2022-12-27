c4e51759f76fa6815e5f96b5ddb55e8801df51eb
Erick Panni <erickpanni@icloud.com>
2022-12-27 19:22:02 -0300

    FEAT+Test[Use-case]: Read notifications

--- src/application/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/application/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -7,19 +7,24 @@ import { PrismaNotificationMapper } from '../mapper/prisma-notification-mapper';
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
-  countManyByRecipientId(recipientId: string): Promise<number> {
-    throw new Error('Method not implemented.');
-  }
-  async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
-  }
-  async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
+
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
     await this.prismaService.notification.create({
       data: raw,
     });
+  }
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+  async findManyByRecipientId(recipientId: any): Promise<Notification[]> {
+    throw new Error('Method not implemented.');
+  }
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
